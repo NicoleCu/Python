63f314d2648b812479216e11611e01dd73f5c201
NicoleCu <32576333+NicoleCu@users.noreply.github.com>
2017-11-24 22:19:05 +0300

    Add files via upload

---
+++ №3 от 18.11.py
@@ -0,0 +1,24 @@
+# вычислить количество сочетаний из n по k
+
+n = input("Факториал числа ")
+n = int(n)
+k = input()
+k = int(k)
+fac1 = 1
+fac2 = 1
+i = 0
+p = 0
+y = 0
+j = n - k
+fac3 = 1
+while i < n:
+     i = i + 1
+     fac1 = fac1 * i
+while p < k:
+     p = p + 1
+     fac2 = fac2 * p
+while y < j:
+     y = y + 1
+     fac3 = fac3 * y
+print(fac1//(fac2*fac3))
+
